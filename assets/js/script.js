10fbec7b6af73ce2bf397eb3205c188c3d7a622a
Jayaram <sriramselvam016@gmail.com>
2025-06-21 18:47:20 +0530

    portfolio website

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(".counter").each(function () {
+    var $this = $(this),
+      countTo = $this.attr("data-countto");
+    countDuration = parseInt($this.attr("data-duration"));
+    $({ counter: $this.text() }).animate(
+      {
+        counter: countTo
+      },
+      {
+        duration: countDuration,
+        easing: "linear",
+        step: function () {
+          $this.text(Math.floor(this.counter));
+        },
+        complete: function () {
+          $this.text(this.counter);
+        }
+      }
+    );
+  });
+  
